6e131897ba80c4d5411035deb9be34c30e12bf9a
istvanpuki <puki.istvan92@gmail.com>
2022-02-07 22:02:34 +0100

    npm run server-rel jó csak a nodemon

--- backend/server.js
+++ backend/server.js
@@ -5,4 +5,8 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use('/api/goals', require('./routes/goalRoutes'))
+
 app.listen(PORT, () => console.log(`Server Started on PORT ${PORT}`))
+
+
